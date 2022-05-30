7753da8b78e77012af7795355546507c21db251c
Yash Agarwal <49517141+agarwalyash02@users.noreply.github.com>
2022-05-30 16:17:07 +0530

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -2,7 +2,7 @@ libraries{
 //   merge = true // allow individual apps to contribute additional libraries
 //   test-search
     maven {
-        envName = "${envName}"
+        envName = "This is ${envName}"
     }
 }
 
