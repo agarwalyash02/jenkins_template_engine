28dc8ca3f187d4c8d5194d76135151188ad5b68f
Yash Agarwal <49517141+agarwalyash02@users.noreply.github.com>
2022-05-30 17:02:32 +0530

    Update build.groovy

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,16 +1,21 @@
 void call(){
     if (config.envName == "dev"){
-        stage('print using echo') {
-            node {
-                
-                sh 'echo helloStarting'
-                println config.envName
-            }
-            node {
-                sh 'echo heyEnding'
-            }
+        environment {
+            service = 'search-dev'
         }
     }
+    stage('print using echo') {
+        node {
+            
+            sh 'echo helloStarting'
+            println config.envName
+        }
+        node {
+            sh 'echo heyEnding'
+            ah 'echo $service'
+        }
+    }
+    
     stage('print env') {
             node {
                 sh 'echo byeStarting'
