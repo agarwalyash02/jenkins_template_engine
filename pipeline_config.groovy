71f71805c57613133d9cadfe8599b7e2cdb9f499
Yash Agarwal <49517141+agarwalyash02@users.noreply.github.com>
2022-06-03 11:50:37 +0530

    changed

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,24 +1,4 @@
-libraries{ // allow individual apps to contribute additional libraries
-//   test-search
-    // maven {
-    //     envName = "${env.envName}"
-    // }
+libraries{
     maven
     search
 }
-
-// application_environments {
-//         // BITBUCKET = credentials('Bitbucket')
-//         // BRANCH_NAME = 'master'
-//         ENV_NAME = 'STAG'
-//         LOGGLY_TAG = 'STAG'
-//         // REDIS_LISTEN_ADDRESS = 'staging-redis.xgwnhr.ng.0001.aps1.cache.amazonaws.com:6379'
-//         // REDIS_MAX_IDLE_CONNECTION = '4'
-//         // REDIS_MAX_ACTIVE_CONNECTION = '16'
-//         // S3_LOCATION_ENV = 'staging'
-//         // DATA_PIPELINE = 'data-pipeline-326614.imphoenix_dev1'
-//         // ES_USERNAME = 'elastic'
-//         // ES_PASSWORD = 'G2u7pDeF9jqOTYTLKGKxc0zv'
-//         // ES_CLOUD_ID = 'dev-es:YXAtc291dGgtMS5hd3MuZWxhc3RpYy1jbG91ZC5jb206OTI0MyQ3NTEzM2E0MTZjYzg0ZjA5OWRhY2EzMTM1ZDNkNmMwMCRiNjY3N2NmNGRmMDA0MTE5YmY0OTI1MDcyZGMyOGM4Yg=='
-//         // SLACK_WEB_HOOK = 'https://hooks.slack.com/services/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'
-// }
